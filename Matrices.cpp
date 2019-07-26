23ac0e523312a2d25f38cf668b136b61d7c0b6d6
JacFox11 <49622962+JacFox11@users.noreply.github.com>
2019-07-26 13:43:42 -0600

    update

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <bits/stdc++.h>
 #ifndef MATRICES_CPP
 #define MATRICES_CPP
 using namespace std;
 
 class Matrices{
 	private:
-		int length;
+		int height;
 		int width;
-		int matriz;
+		int ** matriz;
 	public:
-		Rectangulo(){
-			matriz=new int *[5];
-			for (int i=0; i<5; i++){
-				matriz[i]= new int[5];
+		Matrices(){
+			matriz=llenarm(matriz, 5, 5);
+		}
+		
+		Matrices(int x, int y){
+			height=x;
+			width=y;
+		}
+		
+		Suma(int ** m){
+			if ()
+		}
+		
+		int ** llenarm(int ** m, int x, int y){
+			m=new int *[x];
+			for (int i=0; i<y; i++){
+				m[i]= new int[y];
 			}
 			
-			
+			srand(time(0));
+			for (int i=0; i<5; i++){
+				for (int j=0; j<5; j++){
+					m[x][y]=(rand() % 20)+1;
+				}
+			}
+			return m;
 		}
 		
 		
